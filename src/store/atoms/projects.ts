c151278e7c9f9847605fe715f7d89b28183ee9d5
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2025-02-11 22:27:55 +0200

    chore: add comment

--- src/store/atoms/projects.ts
+++ src/store/atoms/projects.ts
@@ -1,29 +1,22 @@
 import { atom } from "jotai";
+import { atomWithImmer } from "jotai-immer";
 
 import { ProjectView } from "@/services";
 
-export const projectsAtom = atom<ProjectView[]>([]);
+export const projectsAtom = atomWithImmer<ProjectView[]>([]);
 
-export const addProjectAtom = atom(
-  null,
-  (get, set, newProject: ProjectView) => {
-    const currentProjects = get(projectsAtom);
+export const addProjectAtom = atom(null, (_, set, newProject: ProjectView) => {
+  set(projectsAtom, (draft) => {
+    draft.push(newProject);
+  });
+});
 
-    console.log({ currentProjects, newProject });
+// check this
+// export const addProjectAtom = atom(
+//   null,
+//   (get, set, newProject: ProjectView) => {
+//     const currentProjects = get(projectsAtom);
 
-    set(projectsAtom, [...currentProjects, newProject]);
-  }
-);
-
-// import { atom } from "jotai";
-// import { atomWithImmer } from "jotai-immer";
-
-// import { ProjectView } from "@/services";
-
-// export const projectsAtom = atomWithImmer<ProjectView[]>([]);
-
-// export const addProjectAtom = atom(null, (_, set, newProject: ProjectView) => {
-//   set(projectsAtom, (draft) => {
-//     draft.push(newProject);
-//   });
-// });
+//     set(projectsAtom, [...currentProjects, newProject]);
+//   }
+// );
