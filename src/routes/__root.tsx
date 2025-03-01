7e0009af5a5ebb5fc9d15bc4a9e1963f17b532bf
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2025-03-01 15:31:42 +0200

    feat: add suspense

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
-import { VStack } from "@chakra-ui/react";
+import { Box, Spinner, VStack } from "@chakra-ui/react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Suspense } from "react";
 
 import { Header } from "@/components/features";
 
@@ -19,17 +20,30 @@ const appName = "ZeroSHA256Chain";
 const Root: React.FC = () => {
   return (
     <VStack background="black" color="white" minHeight="100vh" width="100vw">
-      <Header
-        routes={routes}
-        appName={appName}
-        p={4}
-        borderBottom="1px solid"
-        borderColor="gray.500"
-      />
+      <Suspense
+        fallback={
+          <Box
+            h="100vh"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Spinner />
+          </Box>
+        }
+      >
+        <Header
+          routes={routes}
+          appName={appName}
+          p={4}
+          borderBottom="1px solid"
+          borderColor="gray.500"
+        />
 
-      <VStack w="fit-content" h="100%" as="main">
-        <Outlet />
-      </VStack>
+        <VStack w="fit-content" h="100%" as="main">
+          <Outlet />
+        </VStack>
+      </Suspense>
     </VStack>
   );
 };
